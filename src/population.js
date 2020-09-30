2ad522fbdfdced5eb836af40b08fbbba279a2854
Daniel Metzler <daniel.metzler@gmail.com>
2020-10-01 00:19:08 +0200

    Added updated population counter.

--- src/population.js
+++ src/population.js
@@ -17,34 +17,55 @@ export function populations() {
     console.log(jsonresponse);
   }); */
 
-  const jsonData = [
-    {
-      Rank: "1",
-      "Country (or dependent territory)": "China[b]",
-      Population: "1,404,658,280",
-      "% of world": "18.0%",
-      Date: "30 Sep 2020",
-      "Source (official or UN)": "National population clock[3]"
-    },
-    {
-      Rank: "2",
-      "Country (or dependent territory)": "India[c]",
-      Population: "1,367,823,887",
-      "% of world": "17.5%",
-      Date: "30 Sep 2020",
-      "Source (official or UN)": "National population clock[4]"
-    }
-  ];
+  function buildOwnMap() {
+    const jsonData = [
+      {
+        Rank: "1",
+        "Country (or dependent territory)": "China[b]",
+        Population: "1,404,658,280",
+        "% of world": "18.0%",
+        Date: "30 Sep 2020",
+        "Source (official or UN)": "National population clock[3]"
+      },
+      {
+        Rank: "2",
+        "Country (or dependent territory)": "India[c]",
+        Population: "1,367,823,887",
+        "% of world": "17.5%",
+        Date: "30 Sep 2020",
+        "Source (official or UN)": "National population clock[4]"
+      }
+    ];
 
-  let populationMap = new Map();
-  for (let i = 0; i < jsonData.length; i++) {
-    const obj = jsonData[i];
-    populationMap.set(obj["Country (or dependent territory)"], obj.Population);
+    let populationMap = new Map();
+    for (let i = 0; i < jsonData.length; i++) {
+      const obj = jsonData[i];
+      populationMap.set(
+          obj["Country (or dependent territory)"],
+          obj.Population
+      );
+    }
+
+    return populationMap;
   }
 
-  for (const [key, value] of populationMap.entries()) {
-    if (key.includes("China")) {
-      console.log(value);
+  function init() {
+    const populationMap = buildOwnMap();
+
+    for (const [key, value] of populationMap.entries()) {
+      if (key.includes("China")) {
+        const currentlyInfected = 366;
+        const deaths = 4739;
+        const population = value.replace(/,/g, "");
+        const infectedPercentage =
+            (currentlyInfected / population).toFixed(12) + "%";
+        const deathPercentage = (deaths / population).toFixed(12) + "%";
+
+        console.log("infectedPercentage: " + infectedPercentage);
+        console.log("deathPercentage: " + deathPercentage);
+      }
     }
   }
+
+  init();
 }
